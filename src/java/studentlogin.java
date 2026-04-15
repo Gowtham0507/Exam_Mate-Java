97d8960bef6279337665c8960f750d71b46ac11d
Gowtham0507 <saigowtham05peddinti@gmail.com>
2026-04-15 17:07:02 +0530

    Fix silent white page errors by catching exceptions properly & fix session thread safety

--- src/java/studentlogin.java
+++ src/java/studentlogin.java
@@ -25,17 +25,7 @@ import java.util.Base64;
  * @author SAURABH
  */
 public class studentlogin extends HttpServlet {
-    HttpSession session;
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -47,6 +37,7 @@ public class studentlogin extends HttpServlet {
 
         try
         {
+            HttpSession session = request.getSession(true);
             MessageDigest MD5=MessageDigest.getInstance("MD5");
             MD5.update(passwd.getBytes(),0,passwd.getBytes().length);
             byte[] hashvalue=MD5.digest();
@@ -54,6 +45,9 @@ public class studentlogin extends HttpServlet {
             
             Config c = new Config();
             Connection con = c.getcon();
+            if (con == null) {
+                throw new Exception("Database connection failed. Check DB variables.");
+            }
             Statement st = con.createStatement();
         
             String selectStatement = "SELECT * FROM student WHERE studentID = ? ";
@@ -62,8 +56,6 @@ public class studentlogin extends HttpServlet {
             rs = prepStmt.executeQuery();
             if(rs.next())
             {
-
-
                 if(newpasswd.equals(rs.getString("password")))
                 {
                     session.setAttribute("username",rs.getString("studentname"));
@@ -84,53 +76,28 @@ public class studentlogin extends HttpServlet {
                response.sendRedirect("index.jsp?RetryStudent=True");
             }
 
-            con.close();
         }
         catch(Exception e)
         {
-            out.println("Error="+e);
+            out.println("<h3>Login Error: " + e.getMessage() + "</h3>");
+            e.printStackTrace();
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        session = request.getSession(true);
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 }
--- src/java/studentregistration.java
+++ src/java/studentregistration.java
@@ -25,16 +25,6 @@ import mail.AuthCode;
  */
 public class studentregistration extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -43,18 +33,12 @@ public class studentregistration extends HttpServlet {
         String rollno=request.getParameter("username");
         String passwd=request.getParameter("passwd");
         String institute=request.getParameter("institute");
-        //String sem=request.getParameter("sem");
         String sem="00";
         String email=request.getParameter("email");
         String number=request.getParameter("number");
-        //String d = request.getParameter("day");
-        //String m = request.getParameter("month");
-        //String y = request.getParameter("year");
-        //String dob = d+"/"+m+"/"+y;
-        //String sex=request.getParameter("sex");
+        String dob="dd/mm/yyyy";
+        String sex="gender";
         ResultSet rs;
-           String dob="dd/mm/yyyy";
-           String sex="gender";
            
         try
         {
@@ -67,6 +51,9 @@ public class studentregistration extends HttpServlet {
             String newpasswd=Base64.getEncoder().encodeToString(hashvalue);
             Config c = new Config();
             Connection con = c.getcon();
+            if (con == null) {
+                throw new Exception("Database connection failed. Check DB variables on Render.");
+            }
             Statement st = con.createStatement();
             String qry = "select count(*) as col from student where studentid ='"+rollno+"'";
             rs = st.executeQuery(qry);
@@ -79,11 +66,6 @@ public class studentregistration extends HttpServlet {
             {
                 String query="insert into student values('"+name+"','"+rollno+"','"+newpasswd+"','"+institute+"','"+sem+"','"+email+"','"+number+"','"+dob+"','"+sex+"','"+code+"')";
                 st.executeUpdate(query);
-                // SMTP server mail.codeshare.in is dead. Disabling MailVerify to prevent connection timeout.
-                // String msg="Your verification CODE is:"+code+" .<br/>To verify Click on this link http://www.codeoj.com/examshow/Verification.jsp?email="+email+"&authcode="+code+"";
-                // System.out.print("msg is" +msg);
-                // MailVerify mv=new MailVerify(email, msg);
-                // mv.verify();
                 con.close();
                 response.sendRedirect("index.jsp?RegisterStudent=True");
             }
@@ -93,52 +75,28 @@ public class studentregistration extends HttpServlet {
                 response.sendRedirect("index.jsp?existsStudent=True");
             }
 
-            // no need to close con again if it's already closed
         }
         catch(Exception e)
         {
-            System.out.println("Error="+e);
+            out.println("<h3>Registration Error: " + e.getMessage() + "</h3>");
+            e.printStackTrace();
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 }
